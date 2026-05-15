5ce936a865c440cce607643d5d14817f8ede6a1a
Ben Brandt <benbrandt22@gmail.com>
2026-05-14 22:34:53 -0500

    Updated device to support RGB/brightness output

--- src/device/src/boot.py
+++ src/device/src/boot.py
@@ -13,10 +13,19 @@ HID_REPORT_DESCRIPTOR = bytes((
     0x81, 0x02,          #     Input(Data, Variable, Absolute, NoWrap, Linear, PreferredState, NoNullPosition, BitField)
     0x75, 0x07,          #     ReportSize(7)
     0x81, 0x03,          #     Input(Constant, Variable, Absolute, NoWrap, Linear, PreferredState, NoNullPosition, BitField)
-    0x09, 0x03,          #     UsageId(LED[0x0003])
-    0x75, 0x01,          #     ReportSize(1)
+    0x09, 0x03,          #     UsageId(RedChannel[0x0003])
+    0x09, 0x04,          #     UsageId(GreenChannel[0x0004])
+    0x09, 0x05,          #     UsageId(BlueChannel[0x0005])
+    0x26, 0xFF, 0x00,    #     LogicalMaximum(255)
+    0x95, 0x03,          #     ReportCount(3)
+    0x75, 0x08,          #     ReportSize(8)
     0x91, 0x02,          #     Output(Data, Variable, Absolute, NoWrap, Linear, PreferredState, NoNullPosition, NonVolatile, BitField)
+    0x09, 0x06,          #     UsageId(Brightness[0x0006])
+    0x25, 0x64,          #     LogicalMaximum(100)
+    0x95, 0x01,          #     ReportCount(1)
     0x75, 0x07,          #     ReportSize(7)
+    0x91, 0x02,          #     Output(Data, Variable, Absolute, NoWrap, Linear, PreferredState, NoNullPosition, NonVolatile, BitField)
+    0x75, 0x01,          #     ReportSize(1)
     0x91, 0x03,          #     Output(Constant, Variable, Absolute, NoWrap, Linear, PreferredState, NoNullPosition, NonVolatile, BitField)
     0xC0,                # EndCollection()
 ))
@@ -27,7 +36,7 @@ my_device = usb_hid.Device(
     usage=0x01,
     report_ids=(1,),
     in_report_lengths=(1,),
-    out_report_lengths=(1,)
+    out_report_lengths=(4,)
 )
 
 usb_hid.enable( (my_device,) )
--- src/device/src/code.py
+++ src/device/src/code.py
@@ -5,7 +5,7 @@ import digitalio
 import neopixel
 import usb_hid
 
-# RP2040-Zero board members:
+# RP2040-Zero board members: (dir(board))
 # ['__class__', '__name__', 'A0', 'A1', 'A2', 'A3', 'GP0', 'GP1', 'GP10', 'GP11', 'GP12', 'GP13', 'GP14', 'GP15', 'GP16', 'GP17', 'GP18', 'GP19', 'GP2', 'GP20', 'GP21', 'GP22', 'GP23', 'GP24', 'GP25', 'GP26', 'GP26_A0', 'GP27', 'GP27_A1', 'GP28', 'GP28_A2', 'GP29', 'GP29_A3', 'GP3', 'GP4', 'GP5', 'GP6', 'GP7', 'GP8', 'GP9', 'NEOPIXEL', 'RX', 'TX', 'UART', '__dict__', 'board_id']
 
 # Get our custom HID device
@@ -32,10 +32,20 @@ last_pressed = False
 
 pixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
 
-def show_color(color):
-    pixel[0] = color
+def show_color(r, g, b, brightness_percent):
+    pixel.brightness = brightness_percent / 100.0
+    pixel[0] = (r, g, b)
 
-show_color((255, 0, 0))
+# ----- STARTUP ANIMATION -----
+
+fadeTimeSec = 0.2
+for i in range(0, 101, 2): # fade in
+    show_color(0, 255, 0, i)
+    time.sleep(fadeTimeSec/50)
+time.sleep(1) #hold
+for i in range(100, -1, -2): # fade out
+    show_color(0, 255, 0, i)
+    time.sleep(fadeTimeSec/50)
 
 # ----- MAIN LOOP -----
 
@@ -58,10 +68,8 @@ while True:
     # --- Receive output report from host ---
     output = device.get_last_received_report(report_id=1)
     if output is not None:
-        command = output[0]
-
-        if command == 0x01:
-            show_color((0, 0, 255))
-        else:
-            show_color((0, 255, 0))
-
+        print(len(output));
+        print(output);
+    if output is not None and len(output) == 4:
+        r, g, b, brightness_percent = output[0], output[1], output[2], output[3]
+        show_color(r, g, b, brightness_percent)
