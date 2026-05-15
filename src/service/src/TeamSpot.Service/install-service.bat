9a43620b0df4f2d35dec33712cde8e43539ebc94
Ben Brandt <benbrandt22@gmail.com>
2026-05-14 23:56:58 -0500

    Updated install script to include service config and startup

--- src/service/src/TeamSpot.Service/install-service.bat
+++ src/service/src/TeamSpot.Service/install-service.bat
@@ -2,9 +2,12 @@
 set "BASE_PATH=%~dp0"
 set "SERVICE_EXE=TeamSpot.Service.exe"
 
-sc.exe create "TeamSpot.Service" binpath="%BASE_PATH%%SERVICE_EXE%"
+sc.exe create "TeamSpot.Service" binpath="%BASE_PATH%%SERVICE_EXE%" start=auto
 sc.exe description "TeamSpot.Service" "Physical interface to MS Teams"
 
-:: TODO: configure service: https://learn.microsoft.com/en-us/dotnet/core/extensions/windows-service#configure-the-windows-service
+:: Service configuration: https://learn.microsoft.com/en-us/dotnet/core/extensions/windows-service#configure-the-windows-service
+sc.exe failure "TeamSpot.Service" reset= 0 actions= restart/60000/restart/60000/run/1000
+
+sc.exe start "TeamSpot.Service"
 
 pause
